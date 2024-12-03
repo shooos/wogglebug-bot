7e60a981c2cd5d3cd7c749ee05c29416a8e67c57
Sho S <wpoqwpoqw@gmail.com>
2024-12-03 20:14:32 +0900

    create clasp project

---
+++ bsky/create-session.ts
@@ -0,0 +1,18 @@
+const CreateSession = (() => {
+    const credencials = {
+        identifier: PropertiesService.getScriptProperties().getProperty('BLUESKY_HANDLE'),
+        password: PropertiesService.getScriptProperties().getProperty('BLUESKY_PASSWORD'),
+    }
+
+    const url = 'https://bsky.social/xrpc/com.atproto.server.createSession';
+
+    return {
+        execute: (): string => {
+            const response = UrlFetchApp.fetch(url, { method: 'post', contentType: 'application/json', payload: JSON.stringify(credencials) });
+            const contentText = response.getContentText();
+            const result = JSON.parse(contentText);
+
+            return result.accessJwt;
+        }
+    }
+})();
---
+++ index.ts
@@ -0,0 +1,34 @@
+/**
+ * HoYoverse 公式情報を収集して Bsky 連携する
+ */
+function subscribeHoYoOfficial(): void {
+
+}
+
+/**
+ * Bsky に毎日定期的にお知らせをポストする
+ */
+function dailyInformationPost(): void {
+
+}
+
+/**
+ * 深境螺旋が開始したことを Bsky にポストする
+ */
+function noticeOpenSpiralAbyss(): void {
+
+}
+
+/**
+ * 深境螺旋がリセット間近であることを Bsky にポストする
+ */
+function noticeCountdownResetSpiralAbyss(): void {
+
+}
+
+/**
+ * スターライト交換が更新されたことを Bsky にポストする
+ */
+function noticeStarlightShopCharcter(): void {
+
+}
