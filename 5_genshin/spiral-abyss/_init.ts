abfb86df05b37e19ee747d312f188d72c1a76845
shooos <wpoqwpoqw@gmail.com>
2024-12-16 00:00:04 +0900

    failed to create image

--- 5_genshin/spiral-abyss/_init.ts
+++ 5_genshin/spiral-abyss/_init.ts
@@ -1,3 +1,32 @@
 (() => {
   Genshin.spiralAbyss = {};
 })();
+
+function clientLogger(message: string): void {
+  Logger.log(message);
+}
+
+function saveSpiralAbyssImage({ fileName, imageType, base64 }: Genshin.SprialAbyssImageData): string {
+  Logger.log(`Start to save spiral abyss image | FileName=${fileName}`);
+
+  const imagesFolder = DriveApp.getFolderById(GENSHIN_SPIRAL_ABYSS_IMAGE_FOLDER_ID);
+
+  const encoded = base64.replace('data:image/png;base64,', '');
+  const decoded = Utilities.base64Decode(encoded);
+  const blob = Utilities.newBlob(decoded, imageType, fileName);
+
+  const created = imagesFolder.createFile(blob);
+  const fileId = created.getId();
+
+  Logger.log(`Completed save spiral abyss image | FileId=${fileId}`);
+
+  return fileId;
+}
+
+function successSaveSpiralAbyssImage(fileId: string) {
+  Logger.log(`Completed save spiral abyss image | FileId=${fileId}`);
+}
+
+function failureSaveSpiralAbyssImage(error: Error) {
+  throw new Error(`Failed to save spiral abyss image | Reason=${error.message}`);
+}
--- 5_genshin/spiral-abyss/create-image.ts
+++ 5_genshin/spiral-abyss/create-image.ts
@@ -5,22 +5,21 @@
   }
 
   Genshin.spiralAbyss.createImage = (hours) => {
+    Logger.log(`Start to create spiral abyss image | Hours=${hours}`);
+
     const image = toDataUrl('1ESTC71ucNOR-ctKaFXYbadzjZfL_ojLH');
-    const hoursPositionX = hours < 10 ? 260 : 340;
+    const hoursPositionX = hours < 10 ? 320 : 260;
 
     const html = `
 <!DOCTYPE html>
 <html lang="ja">
   <head>
+    <base target="_top">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <canvas id="canvas" style="width: 768px; height: 512px;"></canvas>
-
     <script>
       function successSaveImage(fileId) {
         google.script.run.successSaveSpiralAbyssImage(fileId);
@@ -31,48 +30,67 @@
       }
 
       function draw() {
+        document.body.append(document.createTextNode('Draw image to canvas'));
+
         const canvas = document.getElementById('canvas');
-          const ctx = canvas.getContext('2d');
-          const baseImage = new Image();
-          const grad=ctx.createLinearGradient(0, 0, 0, 400);
-          grad.addColorStop(0, 'yellow');
-          grad.addColorStop(1, 'orange');
-          
-          baseImage.onload = function() {
-            canvas.width = this.naturalWidth;
-            canvas.height = this.naturalHeight;
-            ctx.drawImage(baseImage, 0, 0, 768, 512);
+        const ctx = canvas.getContext('2d');
+        const baseImage = new Image();
+        const grad=ctx.createLinearGradient(0, 0, 0, 400);
+        grad.addColorStop(0, 'yellow');
+        grad.addColorStop(1, 'orange');
+        
+        baseImage.onload = function() {
+          document.body.append(document.createTextNode('On load image'));
 
-            ctx.font = '110px "Yusei Magic"';
-            ctx.fillStyle = 'white';
-            ctx.fillText('あと', 70, 380);
-            ctx.fillText('時間', 520, 380);
+          canvas.width = this.naturalWidth;
+          canvas.height = this.naturalHeight;
+          ctx.drawImage(baseImage, 0, 0, 768, 512);
 
-            ctx.font = '240px "Yusei Magic"';
-            ctx.fillStyle = grad;
-            ctx.fillText(${hours}, ${hoursPositionX}, 380);
+          ctx.font = '110px "Yusei Magic"';
+          ctx.fillStyle = 'white';
+          ctx.fillText('あと', 70, 380);
+          ctx.fillText('時間', 520, 380);
 
-            const fileName = 'spiral-abyss.png';
-            const imageType = 'image/png';
-            const base64 = board.toDataURL(imageType);
-            const imageData = { fileName, imageType, base64 };
+          ctx.font = '240px "Yusei Magic"';
+          ctx.fillStyle = grad;
+          ctx.fillText(${hours}, ${hoursPositionX}, 380);
 
-            google.script.run
-              .withSuccessHandler(successSaveImage)
-              .withFailureHandler(failureSaveImage)
-              .saveSpiralAbyssImage(imageData);
+          const fileName = 'spiral-abyss.png';
+          const imageType = 'image/png';
+          const base64 = canvas.toDataURL(imageType);
+          const imageData = { fileName, imageType, base64 };
+
+          document.body.append(document.createTextNode('Created image base64'));
+
+          try {
+          google.script.run
+            .withSuccessHandler(successSaveImage)
+            .withFailureHandler(failureSaveImage)
+            .saveSpiralAbyssImage(imageData);
+          }catch(e){
+            document.body.append(document.createTextNode(e));
           }
-          baseImage.src = ${image}
+        }
+        baseImage.src = '${image}';
       }
 
-      draw()
+      window.onload = () => { draw() }
     </script>
+  </head>
+  <body>
+    <canvas id="canvas" style="width: 768px; height: 512px;"></canvas>
   </body>
 </html>
     `;
 
+    const output = HtmlService.createHtmlOutput(html);
+    const blob = output.getBlob().setName('spiral-abyss.pdf');
+    DriveApp.createFile(blob.getAs(MimeType.PDF));
+
     const imageFolder = DriveApp.getFolderById(GENSHIN_SPIRAL_ABYSS_IMAGE_FOLDER_ID);
     const imageFile = imageFolder.getFiles().next();
+
+    Logger.log(`Completed create spiral abyss image`);
 
     return imageFile;
   }
