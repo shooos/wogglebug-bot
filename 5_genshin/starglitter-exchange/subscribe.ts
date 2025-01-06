ae1ca9a4313c412590342a087758a79092040812
shooos <wpoqwpoqw@gmail.com>
2025-01-06 23:25:39 +0900

    filter

--- 5_genshin/starglitter-exchange/subscribe.ts
+++ 5_genshin/starglitter-exchange/subscribe.ts
@@ -2,7 +2,7 @@
   Genshin.starglitterExchange.subscribe = (currentDate) => {
     Logger.log(`Subscribe starglitter exchange characters`);
 
-    if (currentDate.getDate() !== 1) {
+    if (currentDate.getDate() !== 1 || currentDate.getHours() !== 5 || currentDate.getMinutes() > 14) {
       Logger.log(`Unsbscribe starglitter exchange characters`);
       return null;
     }
