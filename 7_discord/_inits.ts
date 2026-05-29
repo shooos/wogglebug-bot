3b2dd267b911d9c919e7b4fb261c2d2a8e630132
Sho S <wpoqwpoqw@gmail.com>
2026-05-29 11:20:00 +0900

    途中

--- 7_discord/_inits.ts
+++ 7_discord/_inits.ts
@@ -1,8 +1,8 @@
 namespace Discord {
   const CLIENT_ID = PropertiesService.getScriptProperties().getProperty('DISCORD_OAUTH2_CLIENT_ID');
   const CLIENT_TOKEN = PropertiesService.getScriptProperties().getProperty('DISCORD_OAUTH2_CLIENT_TOKEN');
-  const BOT_TOKEN = PropertiesService.getScriptProperties().getProperty('DISCORD_BOT_TOKEN');
-  const BASE_URL = 'https://discord.com/api/v10';
+  export const BOT_TOKEN = PropertiesService.getScriptProperties().getProperty('DISCORD_BOT_TOKEN');
+  export const BASE_URL = 'https://discord.com/api/v10';
 
   export interface Message {
     id: string;
@@ -10,7 +10,7 @@ namespace Discord {
   }
 
   export interface Functions {
-    getMessages(channelId: string, after: Date): Message[];
+    getMessages(channelId: string, after: string): Message[];
   }
 }
 
--- 8_neverness-to-everness/build-message.ts
+++ 8_neverness-to-everness/build-message.ts
@@ -1,1 +1,14 @@
-(() => { })();
+(() => {
+  Nte.buildMessage = (message: Discord.Message): Bluesky.Message => {
+    const content = message.content;
+    const author = message.author;
+    const channel = message.channel;
+
+    return {
+      content,
+      author,
+      channel,
+      timestamp: new Date(),
+    };
+  };
+})();
